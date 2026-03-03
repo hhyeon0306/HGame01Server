dc1b5ffe9e95b166b5af4fd40bf3530ad97d34d3
hhyeon0306 <251015779+hhyeon0306@users.noreply.github.com>
2026-03-04 05:06:03 +0900

    feat: 인증 미들웨어 (토큰 검증 + 유저 데이터 로드)

---
+++ HGame01Server/Middleware/CheckUserAuthAndLoadUserData.cs
@@ -0,0 +1,130 @@
+using System.Text.Json;
+using HGame01Server.Repository;
+
+namespace HGame01Server.Middleware;
+
+public class CheckUserAuthAndLoadUserData
+{
+    readonly IMemoryDB _memoryDB;
+    readonly RequestDelegate _next;
+
+    public CheckUserAuthAndLoadUserData(RequestDelegate next, IMemoryDB memoryDB)
+    {
+        _memoryDB = memoryDB;
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        // 로그인, 회원가입 api는 토큰 검사를 하지 않는다.
+        var formString = context.Request.Path.Value;
+        if (formString.Contains("/api/Login", StringComparison.OrdinalIgnoreCase) ||
+            formString.Contains("/api/CreateAccount", StringComparison.OrdinalIgnoreCase) ||
+            formString.Contains("/api/Admin", StringComparison.OrdinalIgnoreCase))
+        {
+            await _next(context);
+            return;
+        }
+
+        // token이 있는지 검사하고 있다면 저장
+        var (isTokenNotExist, token) = await IsTokenNotExistOrReturnToken(context);
+        if (isTokenNotExist)
+        {
+            return;
+        }
+
+        // user_id가 있는지 검사하고 있다면 저장
+        var (isUserIDNotExist, user_id) = await IsUserIDNotExistOrReturnUserID(context);
+        if (isUserIDNotExist)
+        {
+            return;
+        }
+
+        // uid를 키로 하는 데이터 없을 때
+        (bool isOk, MdbUserData userInfo) = await _memoryDB.GetUserAsync(user_id);
+        if (isOk == false)
+        {
+            await ResponseInvalidUserAuthToken(context);
+            return;
+        }
+
+        // 토큰이 일치하지 않을 때
+        if (await IsInvalidUserAuthTokenThenSendError(context, userInfo, token))
+        {
+            return;
+        }
+
+        context.Items[nameof(MdbUserData)] = userInfo;
+
+        // Call the next delegate/middleware in the pipeline
+        await _next(context);
+    }
+
+    async Task<(bool, string)> IsTokenNotExistOrReturnToken(HttpContext context)
+    {
+        if (context.Request.Headers.TryGetValue("authtoken", out var token))
+        {
+            return (false, token);
+        }
+
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        var errorJsonResponse = JsonSerializer.Serialize(new MiddlewareResponse
+        {
+            result = ErrorCode.TokenDoesNotExist
+        });
+        await context.Response.WriteAsync(errorJsonResponse);
+
+        return (true, "");
+    }
+
+    async Task<(bool, string)> IsUserIDNotExistOrReturnUserID(HttpContext context)
+    {
+        // 대문자를 사용해서 보내어도 소문자로 인코딩 되므로 소문자로 검색한다
+        if (context.Request.Headers.TryGetValue("userid", out var uid))
+        {
+            return (false, uid);
+        }
+
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        var errorJsonResponse = JsonSerializer.Serialize(new MiddlewareResponse
+        {
+            result = ErrorCode.UserIDDoesNotExist
+        });
+        await context.Response.WriteAsync(errorJsonResponse);
+
+        return (true, "");
+    }
+
+    async Task<bool> IsInvalidUserAuthTokenThenSendError(HttpContext context, MdbUserData userInfo, string token)
+    {
+        if (string.CompareOrdinal(userInfo.AuthToken, token) == 0)
+        {
+            return false;
+        }
+
+        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        var errorJsonResponse = JsonSerializer.Serialize(new MiddlewareResponse
+        {
+            result = ErrorCode.AuthTokenFailWrongAuthToken
+        });
+        await context.Response.WriteAsync(errorJsonResponse);
+
+        return true;
+    }
+
+    async Task ResponseInvalidUserAuthToken(HttpContext context)
+    {
+        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        var errorJsonResponse = JsonSerializer.Serialize(new MiddlewareResponse
+        {
+            result = ErrorCode.AuthTokenKeyNotFound
+        });
+
+        await context.Response.WriteAsync(errorJsonResponse);
+    }
+
+    class MiddlewareResponse
+    {
+        public ErrorCode result { get; set; }
+    }
+}
